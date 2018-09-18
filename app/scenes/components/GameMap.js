1e7c08e8887c79a1d79480defe831824fd6258f8
rootasjey <jeremiecorpinot@outlook.com>
2018-09-19 00:32:01 +0200

    Refactoring

---
+++ app/scenes/components/GameMap.js
@@ -0,0 +1,361 @@
+import { unitsFactory } from '../../objects/unitsFactory';
+
+export default class GameMap extends Phaser.GameObjects.GameObject {
+  constructor(scene) {
+    super(scene, 'GameMap');
+
+    this.cursor = {};
+
+    this.layers = {
+      carpet: { },
+      characters: { },
+      collision: { },
+      cursor: { },
+      details: { },
+      floor: { },
+      hiddenFloor: { },
+      movement: { },
+      objects: { },
+      uiTileInfo: { },
+    };
+
+    this.map = {};
+    this.selectedCharacter = {};
+    this.tilesMovement = [];
+
+    this.tileset = {
+      characters: { },
+      map: { },
+      ui: { }
+    };
+  }
+
+  addTilesetImages() {
+    const { map, tileset } = this;
+
+    tileset.map = map.addTilesetImage('terrain', 'mapTileset');
+    tileset.characters = map.addTilesetImage('characters', 'charactersTileset');
+    tileset.ui = map.addTilesetImage('ui', 'uiTileset');
+
+    return this;
+  }
+
+  createMapCursor() {
+    this.cursor = this.layers.cursor.getTileAt(0, 0);
+
+    return this;
+  }
+
+  createDynamicLayer() {
+    const { layers, map, tileset } = this;
+
+    layers.collision = map.createDynamicLayer('Collision', tileset.map);
+    layers.movement = map.createDynamicLayer('Movement', tileset.map);
+    layers.characters = map.createDynamicLayer('Characters', tileset.characters);
+    layers.cursor = map.createDynamicLayer('Cursor', tileset.map);
+    layers.uiTileInfo = map.createDynamicLayer('UITileInfo', tileset.ui);
+
+    return this;
+  }
+
+  createMap() {
+    this.map = this.scene.make.tilemap({ key: 'level0' });
+
+    return this;
+  }
+
+  createStaticLayers() {
+    const { layers, map, tileset } = this;
+
+    layers.floor = map.createStaticLayer('Floor', tileset.map);
+    layers.hiddenFloor = map.createStaticLayer('HiddenFloor', tileset.map);
+    layers.carpet = map.createStaticLayer('Carpet', tileset.map);
+    layers.objects = map.createStaticLayer('Objects', tileset.map);
+    layers.details = map.createStaticLayer('Details', tileset.map);
+
+    return this;
+  }
+
+  createUnits() {
+    const { scene } = this;
+    const layer = this.layers.characters;
+
+    this.createUnit = unitsFactory({
+      dataHeroes: scene.cache.json.get('heroes'),
+      dataUnit: scene.cache.json.get('units'),
+      dataWeapons: scene.cache.json.get('weapons'),
+      dataConsummables: scene.cache.json.get('consummables')
+    });
+
+    const buildUnit = (tile) => {
+      tile.properties.unit = this.createUnit(tile.properties.unitName);
+    };
+
+    layer.forEachTile(buildUnit, undefined, 0, 0,
+      undefined, undefined, { isNotEmpty: true });
+
+    return this;
+  }
+
+  disableEvents() {
+    const { scene } = this;
+
+    scene.input.off('pointerdown', this.onPointerDown);
+    scene.input.off('pointermove', this.onPointerMove);
+
+    scene.input.keyboard.off('keydown_UP', this.onMoveCursorUp);
+    scene.input.keyboard.off('keydown_DOWN', this.onMoveCursorDown);
+    scene.input.keyboard.off('keydown_LEFT', this.onMoveCursorLeft);
+    scene.input.keyboard.off('keydown_RIGHT', this.onMoveCursorRight);
+
+    return this;
+  }
+
+  enableEvents() {
+    const { scene } = this;
+
+    scene.input.on('pointerdown', this.onPointerDown);
+    scene.input.on('pointermove', this.onPointerMove);
+
+    scene.input.keyboard.on('keydown_UP', this.onMoveCursorUp);
+    scene.input.keyboard.on('keydown_DOWN', this.onMoveCursorDown);
+    scene.input.keyboard.on('keydown_LEFT', this.onMoveCursorLeft);
+    scene.input.keyboard.on('keydown_RIGHT', this.onMoveCursorRight);
+
+    return this;
+  }
+
+  /**
+   * Initialize map, layers, units and events.
+   */
+  init() {
+    this.createMap()
+      .addTilesetImages()
+      .createStaticLayers()
+      .createDynamicLayer()
+      .scaleToGameSize()
+      .createMapCursor()
+      .createUnits()
+      .listenToEvents();
+  }
+
+  listenToEvents() {
+    const { scene } = this;
+
+    scene.events.on('subscribeMapEvents', this.enableEvents);
+    scene.events.on('unsubscribeMapEvents', this.disableEvents);
+
+    this.enableEvents();
+  }
+
+  onMoveCursorUp() {
+    const { gameMap } = this.scene;
+    const { x, y } = gameMap.cursor;
+
+    const previousY = y - 1;
+
+    if (previousY <= gameMap.layers.cursor.layer.y) return;
+
+    gameMap.layers.cursor.removeTileAt(x, y);
+    gameMap.cursor = gameMap.layers.cursor.putTileAt(gameMap.cursor, x, previousY);
+
+    this.scene.events.emit('cursorMoved', gameMap.cursor, this.scene);
+  }
+
+  onMoveCursorDown() {
+    const { gameMap } = this.scene;
+    const { x, y } = gameMap.cursor;
+
+    const nextY = y + 1;
+
+    if (nextY >= gameMap.layers.cursor.layer.height) return;
+
+    gameMap.layers.cursor.removeTileAt(x, y);
+    gameMap.cursor = gameMap.layers.cursor.putTileAt(gameMap.cursor, x, nextY);
+
+    this.scene.events.emit('cursorMoved', gameMap.cursor, this.scene);
+  }
+
+  onMoveCursorLeft() {
+    const { gameMap } = this.scene;
+    const { x, y } = gameMap.cursor;
+
+    const previousX = x - 1;
+
+    if (previousX <= gameMap.layers.cursor.layer.x) return;
+
+    gameMap.layers.cursor.removeTileAt(x, y);
+    gameMap.cursor = gameMap.layers.cursor.putTileAt(gameMap.cursor, previousX, y);
+
+    this.scene.events.emit('cursorMoved', gameMap.cursor, this.scene);
+  }
+
+  onMoveCursorRight() {
+    const { gameMap } = this.scene;
+    const { x, y } = gameMap.cursor;
+
+    const nextX = x + 1;
+
+    if (nextX >= gameMap.layers.cursor.layer.width) return;
+
+    gameMap.layers.cursor.removeTileAt(x, y);
+    gameMap.cursor = gameMap.layers.cursor.putTileAt(gameMap.cursor, nextX, y);
+
+    this.scene.events.emit('cursorMoved', gameMap.cursor, this.scene);
+  }
+
+  onPointerDown() {
+    this.scene.gameMap.interactWithCharacter();
+  }
+
+  onPointerMove(pointer) {
+    const cursorLayer = this.scene.gameMap.layers.cursor;
+    let tileCursor = this.scene.gameMap.cursor;
+
+    const { x, y } = pointer;
+
+    // Out of boundaries
+    if (x >= cursorLayer.displayWidth ||
+      y >= cursorLayer.displayHeight) {
+      return;
+    }
+
+    if (!cursorLayer.hasTileAtWorldXY(x, y)) {
+      cursorLayer.removeTileAt(tileCursor.x, tileCursor.y);
+      this.scene.gameMap.cursor = cursorLayer.putTileAtWorldXY(tileCursor, x, y);
+
+      this.scene.events.emit('cursorMoved', tileCursor, this.scene);
+    }
+  }
+
+  /**
+   * Fired when a character receives a pointer event.
+   */
+  interactWithCharacter() {
+    const { x, y } = this.cursor;
+
+    if (this.selectedCharacter) {
+      this.moveCharacterTo(x, y);
+
+      this.tilesMovement =
+        this.hideAllowedMovement(this.layers.movement, this.tilesMovement);
+
+      this.selectedCharacter = null;
+      return;
+    }
+
+    const tileCharacter = this.layers.characters.getTileAt(x, y);
+
+    if (!tileCharacter) return;
+
+    this.selectedCharacter = tileCharacter;
+    this.showAllowedMovement(tileCharacter);
+  }
+
+  /**
+   * Move the selected character to the coordinates.
+   * @param {Number} x x coordinate to move the selected character to.
+   * @param {Number} y y coordinate to move the selected character to.
+   */
+  moveCharacterTo(x, y) {
+    if (!this.layers.movement.hasTileAt(x, y)) return;
+
+    const selectedMovementTile = this.layers.movement.getTileAt(x, y);
+
+    this.layers.characters.removeTileAt(this.selectedCharacter.x, this.selectedCharacter.y);
+    this.layers.characters.putTileAt(this.selectedCharacter,
+      selectedMovementTile.x, selectedMovementTile.y);
+  }
+
+  /**
+   * Hide the allowed movement of the last selected character.
+   * @param {Phaser.Tilemaps.DynamicTilemapLayer|Phaser.Tilemaps.StaticTilemapLayer} layer Layer to remove the tiles from.
+   * @param {Array<Phaser.Tilemaps.Tile>} tilesArray Array containing the tiles to remove.
+   */
+  hideAllowedMovement(layer, tilesArray) {
+    tilesArray.map((tile) => {
+      layer.removeTileAt(tile.x, tile.y);
+    });
+
+    return [];
+  }
+
+  /**
+   * Show the allowed movement for the target character tile.
+   * @param {Phaser.Tilemaps.Tile} tileCharacter Tile character to move.
+   */
+  showAllowedMovement(tileCharacter) {
+    const { unit } = tileCharacter.properties;
+    const move = unit.get('move');
+
+    if (!move) return;
+
+    const coord = {
+      x: tileCharacter.x,
+      y: tileCharacter.y
+    };
+
+    const remainingMove = move + 1;
+
+    this.findValidNeighbours(coord, remainingMove);
+  }
+
+  /**
+   * Find the adjacent allowed movement and add the tiles found to a layer and an array.
+   * @param {coordinates} param0 Coordinate to check the adjacent tile movement.
+   * @param {Number} param0.x X coordinate.
+   * @param {Number} param0.y Y coordinate.
+   * @param {Number} remainingMove Max character's movement.
+   */
+  findValidNeighbours({ x, y }, remainingMove) {
+    if (remainingMove === 0) return;
+
+    // 1.Bounds check
+    if (x >= this.layers.movement.tilemap.width ||
+      y >= this.layers.movement.tilemap.height ||
+      x < 0 || y < 0) {
+      return;
+    }
+
+    // 2.Collision Environment check
+    if (this.layers.collision.hasTileAt(x, y)) return;
+
+    // 3.Collision Character check
+    const character = this.layers.characters.getTileAt(x, y);
+
+    if (character &&
+      character.x !== this.selectedCharacter.x &&
+      character.y !== this.selectedCharacter.y) {
+      return;
+    }
+
+    // 4.Avoid tile duplication
+    if (!this.layers.movement.hasTileAt(x, y)) {
+      const tileMovement = this.layers.movement.putTileAt(this.cursor, x, y);
+      this.tilesMovement.push(tileMovement);
+    }
+
+    const newRemainingMove = remainingMove - 1;
+
+    const coordUp = { x, y: y - 1 };
+    const coordDown = { x, y: y + 1 };
+    const coordLeft = { x: x - 1, y };
+    const coordRight = { x: x + 1, y };
+
+    this.findValidNeighbours(coordUp, newRemainingMove);
+    this.findValidNeighbours(coordDown, newRemainingMove);
+    this.findValidNeighbours(coordLeft, newRemainingMove);
+    this.findValidNeighbours(coordRight, newRemainingMove);
+  }
+
+  scaleToGameSize() {
+    const { height, width } = window.game.config;
+    const { layers } = this;
+
+    Object
+      .values(layers)
+      .map((layer) => layer.setDisplaySize(height, width));
+
+    return this;
+  }
+}
---
+++ app/scenes/components/MapUI.js
@@ -0,0 +1,230 @@
+export default class MapUI extends Phaser.GameObjects.GameObject {
+  constructor(scene) {
+    super(scene, 'MapUI');
+
+    this.texts = {
+      avo: { },
+      def: { },
+      name: { }
+    };
+
+    this.location = 'right';
+
+    this.coord = {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    };
+  }
+
+  createTileInfoUI() {
+    const { left, top } = this.coord;
+
+    const { x, y } = this.scene.gameMap.layers.uiTileInfo.tileToWorldXY(left, top);
+    const pixelX = x + 20;
+    const pixelY = y + 10;
+
+    const style = { fontFamily: 'Kenney Pixel', fontSize: 30 };
+
+    this.texts.name = this.scene.add.text(pixelX, pixelY, ' - ', Object.assign({}, style, { fontSize: 40 }));
+    this.texts.def = this.scene.add.text(pixelX, pixelY + 50, 'DEF. ', style);
+    this.texts.avo = this.scene.add.text(pixelX, pixelY + 70, 'AVO. ', style);
+
+    return this;
+  }
+
+  findTileInfoUIBounds() {
+    const { uiTileInfo } = this.scene.gameMap.layers;
+    let { coord } = this;
+
+    const { x, y } = uiTileInfo.findTile(
+      tile => typeof tile === 'object',
+      undefined, undefined, undefined, undefined, undefined, { isNotEmpty: true });
+
+    coord.top = y;
+    coord.left = x;
+
+    for (let coordX = x; uiTileInfo.hasTileAt(coordX, y); coordX++) {
+      coord.right = coordX;
+    }
+
+    for (let coordY = y; uiTileInfo.hasTileAt(x, coordY); coordY++) {
+      coord.bottom = coordY;
+    }
+
+    return this;
+  }
+
+  listenToEvents() {
+    const { scene } = this;
+
+    scene.events.on('subscribeMapUIEvents', this.enableEvents);
+    scene.events.on('unsubscribeMapUIEvents', this.disableEvents);
+
+    this.enableEvents();
+  }
+
+  enableEvents() {
+    this.scene.events.on('cursorMoved', this.updateTileInfo);
+  }
+
+  disableEvents() {
+    this.scene.events.off('cursorMoved', this.updateTileInfo);
+  }
+
+  init() {
+    this.findTileInfoUIBounds()
+      .createTileInfoUI()
+      .listenToEvents();
+  }
+
+  /**
+   * Display tile information
+   * along side characters and objects info.
+   */
+  updateTileInfo(tileCursor, scene) {
+    const { gameMap, mapUI } = scene;
+
+    const info = mapUI.getTileInfo(tileCursor, gameMap);
+
+    mapUI.drawTileInfo(info)
+      .checkTileInfoXY(tileCursor, gameMap.layers.uiTileInfo);
+  }
+
+  /**
+   * Return the current highlighted tile information.
+   * @return {Object} tile information.
+   */
+  getTileInfo({x, y}, {layers}) {
+    const defaultTileValues = {
+      name: ' - ',
+      avo: 0,
+      def: 0
+    };
+
+    let tileValues = Object.assign({}, defaultTileValues);
+
+    let tile = {};
+
+    if (layers.objects.hasTileAt(x, y)) {
+      tile = layers.objects.getTileAt(x, y);
+
+    } else if (layers.hiddenFloor.hasTileAt(x, y)) {
+      tile = layers.hiddenFloor.getTileAt(x, y);
+
+    } else if (layers.carpet.hasTileAt(x, y)) {
+      tile = layers.carpet.getTileAt(x, y);
+
+    } else if (layers.floor.hasTileAt(x, y)) {
+      tile = layers.floor.getTileAt(x, y);
+    }
+
+    if (tile) {
+      const { properties } = tile;
+      tileValues = Object.assign({}, tileValues, properties);
+    }
+
+    return tileValues;
+  }
+
+  /**
+   * Draw tile information to the screen.
+   * @param {Object} info Tile information.
+   */
+  drawTileInfo(info = {}) {
+    this.texts.name.setText(info.name);
+    this.texts.def.setText('DEF. ' + info.def);
+    this.texts.avo.setText('AVO. ' + info.avo);
+
+    return this;
+  }
+
+  /**
+   * Check tile info XY coord.
+   */
+  checkTileInfoXY({ x, y }, uiTileInfo) {
+    const { coord } = this;
+    const distance = 3;
+
+    const isCloseLeft = Math.abs(x - coord.left) <= distance;
+    const isCloseRight = Math.abs(x - coord.right) <= distance;
+    const isCloseTop = Math.abs(y - coord.top) <= distance;
+    const isCloseBottom = Math.abs(y - coord.bottom) <= distance;
+
+    if (isCloseLeft && isCloseBottom) {
+      this.movePanel(uiTileInfo);
+      return;
+    }
+
+    if (isCloseLeft && isCloseTop) {
+      this.movePanel(uiTileInfo);
+      return;
+    }
+
+    if (isCloseRight && isCloseTop) {
+      this.movePanel(uiTileInfo);
+      return;
+    }
+
+    if (isCloseRight && isCloseBottom) {
+      this.movePanel(uiTileInfo);
+      return;
+    }
+  }
+
+  movePanel(uiTileInfo) {
+    const { coord } = this;
+    let newStartX = 0;
+    let newStartY = 0;
+
+    let { location } = this;
+
+    if (location === 'left') {
+      newStartX = 21;
+      newStartY = 1;
+
+      this.location = 'right';
+    }
+
+    if (location === 'right') {
+      newStartX = 1;
+      newStartY = 1;
+
+      this.location = 'left';
+    }
+
+    uiTileInfo.copy(
+      coord.left,
+      coord.top,
+      coord.right - coord.left + 1,
+      coord.bottom - coord.top + 1,
+      newStartX,
+      newStartY);
+
+    uiTileInfo.forEachTile((tile) => {
+      uiTileInfo.removeTileAt(tile.x, tile.y);
+
+    }, undefined,
+    coord.left,
+    coord.top,
+    coord.right - coord.left + 1,
+    coord.bottom - coord.top + 1);
+
+    this.findTileInfoUIBounds();
+
+    this.updateTileInfoTextPosition(uiTileInfo);
+  }
+
+  updateTileInfoTextPosition(uiTileInfo) {
+    const { left, top } = this.coord;
+
+    const { x, y } = uiTileInfo.tileToWorldXY(left, top);
+    const pixelX = x + 20;
+    const pixelY = y + 10;
+
+    this.texts.name.setPosition(pixelX, pixelY);
+    this.texts.def.setPosition(pixelX, pixelY + 50);
+    this.texts.avo.setPosition(pixelX, pixelY + 70);
+  }
+}
